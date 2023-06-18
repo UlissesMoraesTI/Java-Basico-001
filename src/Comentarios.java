6f9b7aefc39956832ff57281a30226e46e82f25c
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-06-18 20:58:46 -0300

    JAVA_001 - Comentários

---
+++ src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Comentário - 1 Linha
+        System.out.println("Comentário de Uma Linha!");
+
+
+        /*
+         *
+         * Comentário - Bloco de Linha
+         *
+         * */
+        System.out.println("Comentário de Bloco de Linhas!!");
+
+
+        /**
+         * Comentário - JAVA DOC
+         */
+        System.out.println("Comentário Java DOC!!!");
+
+    }
+}
