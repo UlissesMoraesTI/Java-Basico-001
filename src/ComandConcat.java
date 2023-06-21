67c73953765124016e871283de1977a9f26d0159
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-06-20 22:43:53 -0300

    JAVA - Comando s.concat

---
+++ src/ComandConcat.java
@@ -0,0 +1,8 @@
+public class ComandConcat {
+
+    public static void main(String[] args) {
+        String s = "Bom dia";
+        System.out.println(s.concat("!!!"));
+        System.out.println(s + "!!!");
+    }
+}
