5017a3c8c6140f5085f2925e3dca1e516a411c5b
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-06-20 22:55:21 -0300

    JAVA - Comando s.startsWith - S.endWith

--- src/ComandstartWith.java
+++ src/ComandstartWith.java
@@ -1,8 +1,10 @@
 public class ComandstartWith {
     public static void main(String[] args) {
-        String s = "Boa";
-        System.out.println(s.startsWith("boa")); // Resultado Falso - COMEÇA COM?
-
+        String s = "Boa Tarde";
+        System.out.println(s.startsWith("boa")); // Resultado False - COMEÇA COM?
         System.out.println(s.toLowerCase().startsWith("boa")); // Resultado true - COMEÇA COM?
+
+        System.out.println(s.endsWith("TARDE")); // Resultado False - TERMINA COM?
+        System.out.println(s.toUpperCase().endsWith("TARDE")); //Resultado true - TERMINA COM?
     }
 }
