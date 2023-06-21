283175b538b8b878b5e7d9cd97f8390ccce9d8bb
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-06-21 04:45:17 -0300

    JAVA Replace, Substring, IndexOf, Contains

---
+++ src/ComandStringContains.java
@@ -0,0 +1,15 @@
+public class ComandStringContains {
+    public static void main(String[] args) {
+        System.out.println("qualquer frase, Texto.".contains("fra")); //Retorno true = Frase contem "fra"
+
+        System.out.println("qualquer frase, Texto.".indexOf("fra")); //Retorno 9 = "fra" inicia a partir da posição 9
+
+        System.out.println("qualquer frase, Texto.".substring(9)); //Retorno "frase, Texto" a partir da posição 9
+
+        System.out.println("qualquer frase, Texto.".substring(9, 14)); //Retorno "frase" a partir da posição 9 tamanho 4
+
+        System.out.println("qualquer frase, Texto.".replace(",", ".")); //Replace
+
+        System.out.println("qualquer frase, Texto.".replace(".", ",")); //Replace
+    }
+}
