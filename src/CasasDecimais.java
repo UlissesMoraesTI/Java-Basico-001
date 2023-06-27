168a63c8e5bc77a39e5f57f87902a8a7ac626880
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-06-27 14:59:50 -0300

    JAVA - Tratamento Casas Decimais

---
+++ src/CasasDecimais.java
@@ -0,0 +1,10 @@
+public class CasasDecimais {
+    public static void main(String[] args) {
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var idade = 33;
+        var salario = 12345.987;
+
+        System.out.printf("O senhor %s %s tem %d anos e ganha R$ %.2f.", nome, sobrenome, idade, salario);
+    }
+}
