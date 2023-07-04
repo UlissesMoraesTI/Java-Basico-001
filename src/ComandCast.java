1f1aa8abf52ec6206b443f694f00325326d2b9e0
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-03 22:13:42 -0300

    JAVA - CAST

--- src/ComandCast.java
+++ src/ComandCast.java
@@ -1,2 +1,10 @@
 public class ComandCast {
+    public static void main(String[] args) {
+
+        double a = 1;
+        System.out.println(a);
+
+        float b = (float) 1.23456788888;
+        System.out.println(b);
+    }
 }
