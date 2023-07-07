ebda0ce8f3630032df3bd01211a1416438a588c2
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 07:03:08 -0300

    JAVA - Comando Math.pow

---
+++ src/ComandMathPow.java
@@ -0,0 +1,9 @@
+public class ComandMathPow {
+    public static void main(String[] args) {
+        int a = 3 * 4 - 10;
+
+        int b = (int) Math.pow(a, 3);
+
+        System.out.println(b);
+    }
+}
