f381e7564e1ef0d445930ea85fd94e7f9a074b0d
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 14:07:05 -0300

    Java - to hex - No Google

---
+++ src/ToHex.java
@@ -0,0 +1,9 @@
+public class ToHex {
+    public static void main(String[] args) {
+        //No Google -> 97 to hex
+        System.out.println('\u0061');
+
+        int a = 97;
+        int b = 'a';
+    }
+}
