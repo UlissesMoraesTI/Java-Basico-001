2594b54bb591fea24dd914a20f564f88835f15dd
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 17:39:09 -0300

    Operador Unario ++ -- incremento e decremento

---
+++ src/Unarios.java
@@ -0,0 +1,13 @@
+public class Unarios {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        a++;
+        System.out.println(a);
+
+        a--;
+        System.out.println(a);
+    }
+
+}
