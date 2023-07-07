9121b7b9d4a1f33854135de7264aace58a56262c
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 10:23:59 -0300

    JAVA - Desafio Aritimético - Math.pow

--- src/DesafioAritmetico.java
+++ src/DesafioAritmetico.java
@@ -1,9 +1,19 @@
 public class DesafioAritmetico {
     public static void main(String[] args) {
-        int a = 3 * 4 - 10;
+        double numA = Math.pow(6 * (3 + 2), 2)  ;
+        double denA = (3 * 2);
 
-        int b = (int) Math.pow(a, 3);
+        double numB = (1 - 5) * (2 - 7);
+        double denB = 2;
 
-        System.out.println(b);
+        double superiorA = numA / denA;
+        double superiorB = Math.pow(numB / denB, 2);
+
+        double superior = Math.pow(superiorA - superiorB, 3);
+        double inferior = Math.pow(10, 3);
+
+        double resultado = superior / inferior;
+
+        System.out.println("O resultado é " + resultado);
     }
 }
