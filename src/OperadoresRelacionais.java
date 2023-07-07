7f7b103fedddf5ef96bffbbb58ba0ff650a4a858
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 14:05:10 -0300

    Operadores Relacionais != == > <

---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        //No Google -> 97 to hex
+        System.out.println('\u0061');
+
+        int a = 97;
+        int b = 'a';
+
+        System.out.println(a == b);
+
+        System.out.println(3 > 4);
+        System.out.println(3 >= 3);
+        System.out.println(3 < 7);
+        System.out.println(30 <= 7);
+        System.out.println(30 != 7);
+    }
+}
