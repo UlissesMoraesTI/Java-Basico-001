7a1c6a2f562c24921def7a616005f2623b2c40bf
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-07 06:49:51 -0300

    JAVA - MODE % - Resto da Divisão

---
+++ src/ModeRestoDivisao.java
@@ -0,0 +1,13 @@
+public class ModeRestoDivisao {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b); //Resto da Divisão
+
+    }
+}
