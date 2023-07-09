ae422714920aabad4d8c6f3520f2f98ad49e42fb
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-09 10:17:24 -0300

    JAVA - While(contador <= 10)

---
+++ src/ComandWhile.java
@@ -0,0 +1,11 @@
+public class ComandWhile {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.println("Java %d" + contador);
+            contador++;
+        }
+    }
+}
