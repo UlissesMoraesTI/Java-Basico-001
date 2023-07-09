a1e475a4c7b5417f80bc38782f621063cba5bc30
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-07-09 11:49:53 -0300

    Desafio Decremento FOR

---
+++ src/DesafioDecrementoFOR.java
@@ -0,0 +1,13 @@
+public class DesafioDecrementoFOR {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >=0 ; i--) {
+            System.out.println(i);
+            i--;
+        }
+
+        for (int i = 10; i >=0 ; i -= 2) {
+            System.out.println(i);
+        }
+    }
+}
